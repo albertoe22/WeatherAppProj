0a95eaef256d9cb996432944ca93b813c25e4683
albertoe22 <38597481+albertoe22@users.noreply.github.com>
2020-09-25 18:40:25 -0700

    New design
    Added unit conversion for metric or imperial through radio buttons

--- practice.js
+++ practice.js
@@ -8,46 +8,97 @@ var lon;
 var lat;
 var api = 'https://api.openweathermap.org/data/2.5/onecall?lat=';
 var apiKey = '&appid=b21af776a512a935904b958e43e53ccf';
-var units =  '&units=imperial';
+var imperial =  '&units=imperial';
+var metric =  '&units=metric';
 
 var aqi = 'https://api.waqi.info/feed/';
 
 var apiKey2 = '/?token=670aa9e79239359c62a4f2aa9af74cbfd9ea730e';
 
-// const colors = {
+var goog = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+var googKey = '&key=AIzaSyCAe_x4UaDGgmy1fcnnznlNZwJcfw7J_VU';
 
-// }
+
+
 
 // Onclick call this function
 function weatherApp() {
     // Get input from text box
     input = document.getElementById("city");
     let url2 = aqi + input.value + apiKey2;
-
+    let radioButtons = document.getElementsByName("options");
+    let units = "";
+    if (radioButtons[0].checked == true) {
+        units = imperial;
+    }
+    else {
+        units = metric;
+    }
     // Read json file of the url, grab data and call the other functions for DOM
+    let url3 = goog + input.value + googKey;
+    $.getJSON(url3, function (obj3) {  
+        lat = obj3.results[0].geometry.location.lat;
+        lon = obj3.results[0].geometry.location.lng;
+    });
+
     $.getJSON(url2, function(obj1){
         let arr = airData(obj1);
-        lat= arr[0]; lon = arr[1]; value = arr[2];
+        value = arr;
+
         let url = api + lat + '&lon=' + lon + apiKey+units;
         $.getJSON(url, function(obj2){
             gotData(obj2, value);       
         });
       });
-      //
 }
 
+function editTemp(data) {
+    weather = data;
+    let temp = weather.current.temp;
+    let feels = "";
+    let radioButtons =document.getElementsByName("options");
+    if (radioButtons[0].checked == true) {
+        document.getElementById("output").innerHTML = (temp).toFixed(2) +  "0&degF ";    
+        feels = weather.current.feels_like.toFixed(1) +"0&degF";
+    }
+    else {
+        document.getElementById("output").innerHTML = (temp).toFixed(2) +  "0&degC";    
+        feels = weather.current.feels_like.toFixed(1) +"0&degC";
+    }
+
+    let tCard = document.getElementById("tCard");
+    let tText = document.getElementById("tText");
+
+    
+    if (temp <= 70) {
+        tText.innerHTML = "Feels Like: " + feels;
+    }
+    else if (temp>=71 && temp<=88) {
+        tCard.style.background = "#FFBA49";
+        tText.innerHTML = "Feels Like: " + feels;
+    }
+    else {
+        tCard.style.background = "#ff3333";
+        tText.innerHTML = "Feels Like: " + feels;
+    }
+
+}
 // Get weather data
 function gotData(data, value) {
     weather = data;
-    let temp = weather.current.temp;
+    editTemp(data);
+
+    //let temp = weather.current.temp;
     let humidity = weather.current.humidity;
-    let uvi = weather.current.uvi.toFixed(0);
-    let feels = weather.current.feels_like +"0&deg";
+    let uvi =   weather.current.uvi.toFixed(0);
+    // let feels = weather.current.feels_like.toFixed(1) +"0&deg";
+    let tCard = document.getElementById("tCard");
+    let tText = document.getElementById("tText");
     let aCard = document.getElementById("aCard");
     let aText = document.getElementById("aText");
     let uCard = document.getElementById("uCard");
     let uText = document.getElementById("uText");
-    
+
 
     if (value <= 50) {
         aCard.style.background = "green";
@@ -65,9 +116,13 @@ function gotData(data, value) {
         aCard.style.background = "#ff3333";
         aText.innerHTML = "Air quality is unhealthy for the general public and sensitive groups";
     }
-    else {
+    else if (value >200) {
         aCard.style.background ="#ac39ac";
         aText.innerHTML = "Health alert: The risk of health effects is increased for everyone";
+    }
+    else {
+        aCard.style.background = "grey";
+        aText.innerHTML = "API does not contain data"
     }
 
     if (uvi < 3 ) {
@@ -95,7 +150,6 @@ function gotData(data, value) {
         uText.innerHTML = "Extra UV protection needed! Apply a minimum of SPF-15 sunscreen generously and cover exposed skin"
     }
     document.getElementById("uvi").innerHTML =  uvi;
-    document.getElementById("output").innerHTML = (temp).toFixed(2) +  " &degF ";
     document.getElementById("humidity").innerHTML = humidity+"%";
     document.getElementById("aqi").innerHTML =  value;
 
@@ -104,9 +158,7 @@ function gotData(data, value) {
 // Gets air quality data
 function airData(data) {
     let result = data;
-    let arr = result.data.city.geo;
-    arr.push(result.data.aqi);
-    return arr;
+    return result.data.aqi;
 }
 
 
@@ -189,6 +241,3 @@ function airData(data) {
 // //{location}/{datetime}
 // var date = '/'+d.toISOString();
 // var apiKey2 = '.json?appid=b21af776a512a935904b958e43e53ccf';
-
-// var goog = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
-// var googKey = '&key=AIzaSyCAe_x4UaDGgmy1fcnnznlNZwJcfw7J_VU';
